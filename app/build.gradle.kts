c7594801b1b67a3952da2dcbe9a4663739f77553
ThIsLinked <kolyalitvintsev@gmail.com>
2023-10-28 17:34:49 +0300

    End test custom self-update function.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         minSdk = 23
         targetSdk = 33
 
-        versionCode = 2310271
+        versionCode = 2310281
         versionName = "4.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
